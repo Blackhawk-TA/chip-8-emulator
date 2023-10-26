5f8860b99108ba89be3fd9cb9751916ab51e02fc
Blackhawk <daniel.peters1@pm.me>
2023-10-26 02:32:22 +0200

    Add TODO for clock speed parameter

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "src/components/stack.h"
 #include "src/components/memory.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) { //TODO: Maybe add parameter for cpu clock speed
 	// Get cli parameter for ROM path
 	if (argc != 2) {
 		printf("Error: Invalid amount of parameters\n");
