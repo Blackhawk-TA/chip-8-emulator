c30020125bddea9ad1053e2b699d98df57f8cd2c
Blackhawk <daniel.peters1@pm.me>
2023-10-26 12:54:50 +0200

    Prevent automated PC increment on fetch

--- src/components/cpu.c
+++ src/components/cpu.c
@@ -19,8 +19,8 @@ void cpu_cycle() {
 }
 
 uint16_t fetch() {
-	uint8_t instruction_left_part = memory_read(program_counter++);
-	uint8_t instruction_right_part = memory_read(program_counter++);
+	uint8_t instruction_left_part = memory_read(program_counter);
+	uint8_t instruction_right_part = memory_read(program_counter + 1);
 
 	uint16_t instruction = (instruction_left_part << 8) | instruction_right_part;
 	return instruction;
@@ -49,6 +49,7 @@ void decode_and_execute(uint16_t instruction) {
 			switch (instruction) {
 				case 0x00E0: // Clear display
 					clear_display();
+					program_counter += 2;
 					break;
 				case 0x00EE: // Subroutine return
 					break;
@@ -56,8 +57,8 @@ void decode_and_execute(uint16_t instruction) {
 					unknown_instruction(opcode);
 			}
 			break;
-		case 0x1: // 1NNN: Jump
-			program_counter = nnn;
+		case 0x1: // 1NNN: Jump, no PC increment
+			program_counter = nnn; //TODO: check if this is correct or if PC should be reset.
 			break;
 		case 0x2:
 			break;
@@ -69,9 +70,11 @@ void decode_and_execute(uint16_t instruction) {
 			break;
 		case 0x6: // 6XNN: Set register VX to value NN
 			registers[x] = nn;
+			program_counter += 2;
 			break;
 		case 0x7: // 7XNN: Adds NN to register VX, ignores overflows
 			registers[x] += nn;
+			program_counter += 2;
 			break;
 		case 0x8:
 			break;
@@ -79,6 +82,7 @@ void decode_and_execute(uint16_t instruction) {
 			break;
 		case 0xA: // ANNN: Sets index register to NNN
 			index_register = nnn;
+			program_counter += 2;
 			break;
 		case 0xB:
 			break;
@@ -86,6 +90,7 @@ void decode_and_execute(uint16_t instruction) {
 			break;
 		case 0xD: // DXYN: Draw n pixels at x, y
 			draw(registers[x] % WIDTH, registers[y] % HEIGHT, n);
+			program_counter += 2;
 			break;
 		case 0xE:
 			break;
