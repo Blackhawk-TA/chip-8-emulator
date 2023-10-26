177428423b568c417b647557dfa5833d2a04cdff
Blackhawk <daniel.peters1@pm.me>
2023-10-26 19:28:00 +0200

    Optimize implementation of draw instruction

--- src/components/display.c
+++ src/components/display.c
@@ -5,39 +5,33 @@
 #include "display.h"
 #include "memory.h"
 
+#define BYTE_LENGTH 8
+
 const uint8_t WIDTH = 64;
 const uint8_t HEIGHT = 32;
 uint8_t display[64][32] = {};
 
-void draw(uint8_t x, uint8_t y, uint8_t n) {
-	uint8_t px_byte, px_bit;
-	uint8_t x_modified = x;
-	uint8_t y_modified = y;
-	registers[0XF] = 0; // Set flag register to 0
+void draw(uint8_t initial_x, uint8_t initial_y, uint8_t n) {
+	uint8_t px_byte, px_bit, x;
+	uint8_t y = initial_y;
+	registers[0XF] = 0; // 0xF is the flag register
 
-	for (uint8_t i = 0; i < n; i++) {
-		px_byte = memory_read(index_register + i);
-		x_modified = x;
+	for (uint8_t byte_index = 0; byte_index < n && y < HEIGHT; byte_index++) {
+		px_byte = memory_read(index_register + byte_index);
+		x = initial_x;
 
-		for(uint8_t bit_index = 0; bit_index < 8; bit_index++) {
+		for(uint8_t bit_index = 0; bit_index < BYTE_LENGTH && x < WIDTH; bit_index++) {
 			px_bit = (px_byte >> (7 - bit_index)) & 0b1; // Get the bits from left to right
 
-			if (px_bit == 1 && display[x_modified][y_modified] == 1) { //TODO optimize
-				display[x_modified][y_modified] = 0;
-				registers[0xF] = 1;
-			} else if (px_bit == 1 && display[x_modified][y_modified] == 0) {
-				display[x_modified][y_modified] = 1;
+			if (px_bit == 1) {
+				if (display[x][y] == 1) {
+					registers[0xF] = 1;
+				}
+				display[x][y] ^= 1;
 			}
-
-			if (x_modified == WIDTH - 1) {
-				break;
-			}
-			x_modified++;
+			x++;
 		}
-		if (y_modified == HEIGHT - 1) {
-			break;
-		}
-		y_modified++;
+		y++;
 	}
 }
 
