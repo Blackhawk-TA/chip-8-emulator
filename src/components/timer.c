1b7a65b59228598bb5b782289cb8c684e8fc9ae6
Blackhawk <daniel.peters1@pm.me>
2023-10-27 14:33:34 +0200

    Fix timer implementation

--- src/components/timer.c
+++ src/components/timer.c
@@ -10,15 +10,11 @@ uint8_t sound_timer = 60;
 void delay_timer_run() {
 	if (delay_timer > 0) {
 		delay_timer--;
-	} else {
-		delay_timer = 60;
 	}
 }
 
 void sound_timer_run() {
 	if (sound_timer > 0) {
 		sound_timer--;
-	} else {
-		sound_timer = 60;
 	}
 }
