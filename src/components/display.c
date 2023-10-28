6ee1ec476a40eecaae47892fe27b7d0587c3e9b7
Blackhawk <daniel.peters1@pm.me>
2023-10-28 18:10:55 +0200

    Fix display clipping

--- src/components/display.c
+++ src/components/display.c
@@ -14,10 +14,10 @@ void draw(uint8_t x, uint8_t y, uint8_t n) {
 	uint8_t *display_px;
 	registers[0XF] = 0; // 0xF is the flag register
 
-	for (uint8_t byte_index = 0; byte_index < n && y < HEIGHT; byte_index++) {
+	for (uint8_t byte_index = 0; byte_index < n && y + byte_index < HEIGHT; byte_index++) {
 		px_byte = memory_read(index_register + byte_index);
 
-		for (uint8_t bit_index = 0; bit_index < BYTE_LENGTH && x < WIDTH; bit_index++) {
+		for (uint8_t bit_index = 0; bit_index < BYTE_LENGTH && x + bit_index < WIDTH; bit_index++) {
 			px_bit = (px_byte >> (7 - bit_index)) & 0b1; // Get the bits from left to right
 
 			if (px_bit == 1) {
